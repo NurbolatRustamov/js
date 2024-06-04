be369bf5abf05f9ee519029823dd6949d548bcbf
NurbolatRustamov <nurbolatrustamov1@gmail.com>
2024-06-04 10:32:25 +0500

    js file

---
+++ index.js
@@ -0,0 +1,38 @@
+const yosh = prompt("yoshiz nechida")
+
+
+switch (yosh) {
+    case '0':
+        console.log("mening yoshim 0-da");
+        break;
+    case '1':
+        console.log("mening yoshim 1-da");
+        break;
+    case '2':
+        console.log("mening yoshim 2-da");
+        break;
+    case '3':
+        console.log("mening yoshim 3-da");
+        break;
+    case '4':
+        console.log("mening yoshim 4-da");
+        break;
+    case '5':
+        console.log("mening yoshim 5-da");
+        break;
+    case '6':
+        console.log("mening yoshim 6-da");
+        break;
+    case '7':
+        console.log("mening yoshim 7-da");
+        break;
+    case '8':
+        console.log("mening yoshim 8-da");
+        break;
+    case '9':
+        console.log("mening yoshim 9-da");
+        break;
+    default:
+        console.log(xato);
+        break;
+}
